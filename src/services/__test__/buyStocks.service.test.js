fd6eeb8cd9cafd8f111e72ff8f07a770965af354
anukritigarg <82015202+anukritigarg@users.noreply.github.com>
2021-07-22 13:36:43 +0530

    (feat)stockroutes

---
+++ src/services/__test__/buyStocks.service.test.js
@@ -0,0 +1,8 @@
+const { buyStockspostService } = require('../buyStocks.service.js');
+const { makeApiCall } = require('../../utils/index.js');
+jest.mock('../../utils/index.js');
+describe('buyStockspostService', () => {
+  it('should pass the test', () => {
+    //TODO
+  });
+});
